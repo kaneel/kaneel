dfb75fa355791eb7b04d52aaa56e644ae3213f69
Guillaume Richard <mynameiskaneel@gmail.com>
2022-02-06 14:27:27 +0000

    add sbin to path

--- myzsh/exports.zsh
+++ myzsh/exports.zsh
@@ -2,8 +2,10 @@ export LC_ALL="en_GB.UTF-8"
 
 export EDITOR="nvim"
 export VISUAL="nvim"
+export LDFLAGS="-L/usr/local/opt/llvm/lib"
+export CPPFLAGS="-I/usr/local/opt/llvm/include"
 
-export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/local/opt/llvm/bin:$PATH
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
