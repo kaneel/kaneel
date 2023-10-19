b76cbe9f4a1d871c842338c5001f812953fcb2e2
Guillaume Richard <mynameiskaneel@gmail.com>
2023-10-19 03:53:59 +0100

    test

--- init.lua
+++ init.lua
@@ -3,19 +3,11 @@ vim.g.mapleader = ","
 -- LAZY STUFF
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github-.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-else 
-	vim.opt.rtp:prepend(lazypath)
-	require("lazy").setup({
-		{ import = "plugins" },
-	})
-	require("core")
+	-- bootstrap lazy.nvim
+	-- stylua: ignore
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({ { import = "plugins" } })
+require("core")
 -- LAZY STUFF END
