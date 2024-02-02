3822bf1bf5c6ef89b1ed406332596edeaf128fc0
Guillaume Richard <kaneel@Guillaumes-MacBook-Pro.local>
2024-02-02 19:16:50 +0000

    Zsh Updates on mac

--- myzsh/exports.zsh
+++ myzsh/exports.zsh
@@ -3,17 +3,24 @@ export LC_ALL="en_US.UTF-8"
 export EDITOR="nvim"
 export VISUAL="nvim"
 
-export LDFLAGS="-L/usr/local/opt/llvm/lib"
-export CPPFLAGS="-I/usr/local/opt/llvm/include"
-
 export TERM=screen-256color
 
 BASE16_SHELL_PATH="$HOME/.config/base16-shell"
+
 [ -n "$PS1" ] && \
   [ -s "$BASE16_SHELL_PATH/profile_helper.sh" ] && \
     source "$BASE16_SHELL_PATH/profile_helper.sh"
 
-export PATH=$HOME/bin:/usr/include:/usr/local/bin:/usr/local/sbin:/usr/local/opt/llvm/bin:/usr/local/go/bin:$PATH
+# Ruby install needed exports
+# export optflags="-Wno-error=implicit-function-declaration"
+# export LDFLAGS="-L/opt/homebrew/opt/libffi/lib"
+# export CPPFLAGS="-I/opt/homebrew/opt/libffi/include"
+# export PKG_CONFIG_PATH="/opt/homebrew/opt/libffi/lib/pkgconfig"
+
+# reclient stuff for ray
+export NINJA_STATUS='[%r processes, %f/%t @ %c/s (%o): %es] '
+
+export PATH=$HOME/bin:/usr/include:/usr/local/bin:/usr/local/sbin:~/rayspace/depot_tools:$PATH
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
--- myzsh/init.zsh
+++ myzsh/init.zsh
@@ -1,2 +1,6 @@
-fortune | cowsay | lolcat
 zstyle ':completion:*' menu select
+source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+source /opt/homebrew/opt/chruby/share/chruby/auto.sh
+chruby ruby-3.3.0
+base16_ashes
+fortune | cowsay | lolcat
