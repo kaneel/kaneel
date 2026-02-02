458daa2eaf7f8019ab3e486085009e13d85ad9aa
Guillaume Richard <mynameiskaneel@gmail.com>
2026-02-02 07:52:15 +0000

    YAGNI

--- myzsh/exports.zsh
+++ myzsh/exports.zsh
@@ -18,15 +18,12 @@ BASE16_SHELL_PATH="$HOME/.config/base16-shell"
 # export PKG_CONFIG_PATH="/opt/homebrew/opt/libffi/lib/pkgconfig"
 #
 
-source $HOME/.heim/heim/env
-
 export PYENV_ROOT="$HOME/.pyenv"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-# export HEIM_HOME="$HOME/rayspace/heim"
 export BUN_INSTALL="$HOME/.bun"
 
 # reclient stuff for ray
